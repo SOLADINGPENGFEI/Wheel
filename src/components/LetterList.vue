33423e60ef5b83fb65c20dc0fbc2346c01e86482
fyr <846456307qq.com>
2019-07-16 21:14:22 +0800

    touch

--- src/components/LetterList.vue
+++ src/components/LetterList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <ul>
+        <ul
+        @touchstart="touchStart"
+        @touchmove="touchMove"
+        @touchend="touchEnd">
             <li v-for="(item,i) in data" :key="i">{{item}}</li>
         </ul>
         <span v-if="isTouch" class="letter">{{current}}</span>
@@ -18,8 +21,27 @@ export default Vue.extend({
         }
     },
     methods:{
-        touchStart(e:Event){
-            //this.isTouch=true
+        touchStart(e:Event):void{
+            this.isTouch=true
+        },
+        touchMove(e:Event):void{
+            //console.log('e...',e.touches[0].pageY);
+            let pageY=e.touches[0].pageY;
+            let letterHeight=0.4*window.innerHeight/750*100;
+            let letterOffsetTop=(window.innerHeight-letterHeight*this.data.length)/2;
+
+            let letterIndex=Math.floor((pageY-letterOffsetTop)/letterHeight);
+
+            //处理上边界
+            if(letterIndex>this.data.length-1){
+                letterIndex=this.data.length-1;
+            }
+            console.log('letter...',this.data[letterIndex])
+        this.current=this.data[letterIndex]
+        },
+        touchEnd(e:Event):void{
+            this.isTouch=false;
+            this.current='';
         }
     }
 })
@@ -44,5 +66,20 @@ ul{
         height:.4rem;
         width:.4rem;
     }
+}
+.letter{
+    display:inline-block;
+    width:1.6rem;
+    height:1.6rem;
+    background: rgba(0,0,0,.6);
+    border-radius: .1rem;
+    top:50%;
+    left:50%;
+    position: fixed;
+    color:#fff;
+    font-size: .8rem;
+    text-align: center;
+    line-height: 1.6rem;
+    transform: translate(-50%,-50%,0)
 }
 </style>
